4e133e541f95f0c09f337db71de018fbd379151f
jiyasavaliya18 <126229948+jiyasavaliya18@users.noreply.github.com>
2023-03-23 12:29:04 +0530

    Add files via upload

---
+++ cube.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void cube(){
+
+	int n;
+	
+	printf("enter value : ");
+     scanf("%d",&n);
+     
+     printf("the cube is %d",n*n*n);
+}
+
+int main(){
+
+   
+ cube();
+ return 0;  
+
+}
+ 
